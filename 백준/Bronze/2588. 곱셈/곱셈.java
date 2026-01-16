89562ee299e870a96e92b76750e9ab588599fa68
kimsihun0928 <5542jgxjjt@privaterelay.appleid.com>
2026-01-16 22:29:02 +0900

    [Bronze III] Title: 곱셈, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = b / 100;  //
+        int y = (b % 100) / 10;  //8
+        int z = (b % 100) % 10;  //3
+
+
+        System.out.println(a*z);
+        System.out.println(a*y);
+        System.out.println(a*x);
+        System.out.println(a*b);
+
+    }
+}
