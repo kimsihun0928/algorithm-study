b8f97bdafed61bc7bad1d22cfd89b8ea36c81bd1
helppy <5542jgxjjt@privaterelay.appleid.com>
2026-01-28 19:33:23 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 744 ms, Memory: 235452 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());  // 테스트 케이스 개수
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write((a + b) + "\n");
+        }
+
+        bw.flush();   // 버퍼에 쌓인 출력 한 번에 내보내기
+        bw.close();
+        br.close();
+    }
+}
