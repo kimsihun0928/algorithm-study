df4d5b5e18953849137a6bff760c2e2a59fa3465
helppy <5542jgxjjt@privaterelay.appleid.com>
2026-02-03 23:43:45 +0900

    [Bronze V] Title: 합, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 1 ; i <= T ; i++) {
+            sum  += i;
+        }
+
+        System.out.println(sum);
+    }
+}
